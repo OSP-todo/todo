e3377a32a697d998bd95e3953a99d4633719092d
hyeppy226 <hyerin226cs@gmail.com>
2021-11-30 20:13:05 +0900

    .

--- App.js
+++ App.js
@@ -1,21 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './src/App';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>todo-list</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
