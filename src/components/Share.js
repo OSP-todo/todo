368227dcf89513cebfeb7222782a35d47ec018b3
suu3 <71166763+suu3@users.noreply.github.com>
2021-12-16 15:00:42 +0900

    Update Share.js

--- src/components/Share.js
+++ src/components/Share.js
@@ -1,10 +1,19 @@
 import {Share} from "react-native";
 
 const onShare = async (tasks) => {
+    //상단 날짜
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = (today.getMonth()+1);
+    var date = today.getDate();
+    if(month < 10) month = '0' + month;
+    if(date < 10) date = '0' + date;
+    var compareToday = year + "/" + month  + "/" + date;
+    const todayTasks = Object.values(tasks).filter(item => item.dueDate==compareToday);
     try {
       const result = await Share.share( 
         { message: 
-          `오늘의 할일 : ${Object.values(tasks).map((item) => item.text)}` 
+          `오늘의 할일 : ${Object.values(todayTasks).map((item) => item.text)}` 
         } ); 
       if (result.action === Share.sharedAction) {
         if (result.activityType) { 
